121b19035389a7c1d7cf697f348198c3fe7a1b0f
matthewmarshdev <matthewmarshdev@gmail.com>
2018-01-15 14:30:53 -0700

    more new pages 1/15/18 230PM

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-
-  //jquery method for fading in the search bar on search screen 
+  //jquery method for fading in the search bar on search screen
   $(".hidden")
     .fadeIn(2000)
     .removeClass("hidden");
-  });
+});
 
+
+
+
+$(document).on("click", "#search", function() {
+
+     function playAudio() {
+      $("audio").play();
+    
+     }
+
+  window.location.href = "main.html";
+});
+
+
+
+ 
