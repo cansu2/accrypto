81e514236e80201be3bb50085c39cd552055f465
Tucker <tuckerk23@gmail.com>
2018-01-17 19:22:36 -0700

    added firebase script tag to html and added firebase js file

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDZQGa83IiumelIE__jceO_zEwoDZXbDw8",
+    authDomain: "accrypto-af254.firebaseapp.com",
+    databaseURL: "https://accrypto-af254.firebaseio.com",
+    projectId: "accrypto-af254",
+    storageBucket: "accrypto-af254.appspot.com",
+    messagingSenderId: "590295681869"
+  };
+  firebase.initializeApp(config);
